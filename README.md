fd74a03acff5b06ebf1441b62cec772de973d608
Mel Lorek <mel.lorek@icloud.com>
2025-12-17 20:18:36 +0100

    einfacher Summentaschenrechner

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Taschenrechner
+
+Ein einfacher Konsolen-Taschenrechner in Python.
+
+## Aktueller Stand
+- Addition zweier Zahlen über Benutzereingabe
+
+## Starten
+```bash
+python3 Taschenrechner.py
+
---
+++ taschenrechner.py
@@ -0,0 +1,8 @@
+zahl1 = input("Gib eine Zahl ein: ")
+zahl2 = input("Gib eine weitere Zahl ein: ")
+
+ergebnis = float(zahl1) + float(zahl2)
+
+print (ergebnis)
+
+####Kommazahlen werden mit einem punkt angegeben.####
